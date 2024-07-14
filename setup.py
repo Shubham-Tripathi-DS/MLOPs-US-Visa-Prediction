f921e52029b3d7edb73c3c183d8b19e1f36688d8
shubham <shubham.tripathi@ltts.com>
2024-07-14 13:15:27 +0530

    requirements.txt file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shubh",
+    author_email="shubhamdata767@gmail.com",
+    packages=find_packages()
+)
